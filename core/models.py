4fcb1a13c8b6701d3680e81a7f003d88d6329f69
ashir <ashirza@gmail.com>
2025-01-31 20:54:31 +0000

    add bootstrap, make the tables a little nicer

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Prediction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team_a_id = models.IntegerField()
+    team_b_id = models.IntegerField()
+    team_a_score = models.IntegerField()
+    team_b_score = models.IntegerField()
+
+
+class GameweekScore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    season = models.IntegerField()
+    gameweek = models.IntegerField()
+    prediction = models.ForeignKey(Prediction, on_delete=models.CASCADE)
+    score = models.IntegerField()
