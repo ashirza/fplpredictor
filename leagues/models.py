6ecdaeb78ab1c93f2e98064dbc406152259f1f7b
ashir <ashirza@gmail.com>
2025-01-31 22:47:37 +0000

    add join code

--- leagues/models.py
+++ leagues/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -5,3 +7,4 @@ from django.db import models
 class League(models.Model):
     name = models.CharField(max_length=64)
     users = models.ManyToManyField(User, related_name="leagues")
+    join_code = models.UUIDField(default=uuid.uuid4, editable=False)
