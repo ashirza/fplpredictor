1854d4d1be1ef02f3f8cc5424fb3ef4e037a737a
ashir <ashirza@gmail.com>
2025-01-31 23:12:30 +0000

    copy share codes to clipboard

--- core/models.py
+++ core/models.py
@@ -1,18 +1,18 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Prediction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    team_a_id = models.IntegerField()
-    team_b_id = models.IntegerField()
-    team_a_score = models.IntegerField()
-    team_b_score = models.IntegerField()
-
-
-class GameweekScore(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    season = models.IntegerField()
-    gameweek = models.IntegerField()
-    prediction = models.ForeignKey(Prediction, on_delete=models.CASCADE)
-    score = models.IntegerField()
+# from django.contrib.auth.models import User
+# from django.db import models
+#
+#
+# class Prediction(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     team_a_id = models.IntegerField()
+#     team_b_id = models.IntegerField()
+#     team_a_score = models.IntegerField()
+#     team_b_score = models.IntegerField()
+#
+#
+# class GameweekScore(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     season = models.IntegerField()
+#     gameweek = models.IntegerField()
+#     prediction = models.ForeignKey(Prediction, on_delete=models.CASCADE)
+#     score = models.IntegerField()
